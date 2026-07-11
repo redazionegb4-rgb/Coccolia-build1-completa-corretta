d8bf3b1cae1476beb9f020a03ca8c73ac411b22b
Demetrio Barreca <demetrio.barreca87@gmail.com>
2026-07-12 01:42:10 +0200

    Initial Commit

---
+++ Coccolia/Resources/AppTheme.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+enum AppTheme {
+ static let page = Color(red:0.985,green:0.965,blue:0.945)
+ static let navy = Color(red:0.20,green:0.18,blue:0.28)
+ static let teal = Color(red:0.84,green:0.48,blue:0.58)
+ static let tealDark = Color(red:0.63,green:0.29,blue:0.42)
+ static let mint = Color(red:0.91,green:0.83,blue:0.75)
+ static let lavender = Color(red:0.87,green:0.81,blue:0.94)
+ static let brandGradient = LinearGradient(colors:[Color(red:0.93,green:0.56,blue:0.66),Color(red:0.68,green:0.53,blue:0.86)],startPoint:.topLeading,endPoint:.bottomTrailing)
+}
